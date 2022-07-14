f9a5d76d99ec7a434451ef151fe5a2356963800f
johnsjohns <o-ana@hotmail.com>
2022-07-13 23:14:04 -0300

    Melhora logica do menu principal

--- jogos.py
+++ jogos.py
@@ -6,20 +6,22 @@ def escolhe_jogo():
     print("*******Escolha o seu jogo!*******")
     print("*********************************")
 
-    print("(1) Forca (2) Adivinhação")
+    while (True):
+        print("(1) Forca (2) Adivinhação")
+        print ("(0) para sair")
 
-    jogo = int(input("Qual jogo? "))
+        jogo = int(input("Qual jogo? "))
 
-    if(jogo == 1):
-        print("Jogando forca")
-        forca.jogar()
-        escolhe_jogo()
-    elif(jogo == 2):
-        print("Jogando adivinhação")
-        adivinhacao.jogar()
-
-        escolhe_jogo()
-
-
+        if(jogo == 1):
+            print("Jogando forca")
+            forca.jogar()
+            escolhe_jogo()
+        elif(jogo == 2):
+            print("Jogando adivinhação")
+            adivinhacao.jogar()
+        elif(jogo == 0):
+            exit(0)
+        print("Pressione ENTER para continuar ...")
+        input()
 if(__name__ == "__main__"):
     escolhe_jogo()
